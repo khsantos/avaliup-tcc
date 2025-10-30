84c46b0c71f3874eaa289342862fb6bf9e81d9b6
Vinicius Ghizani <99882594+ViniGhizani@users.noreply.github.com>
2025-10-29 21:21:47 -0300

    arrumando tamanho header

--- src/components/Header/mobile-drawer.tsx
+++ src/components/Header/mobile-drawer.tsx
@@ -3,10 +3,12 @@
 import { useRef } from "react";
 import { NAV } from "./nav/nav.config";
 import { NavSection } from "./nav-section";
-import { Logo } from "./logo";
 import { UserMenu } from "./user-menu";
 import { useLockBodyScroll } from "../../hooks/useLockBodyScroll";
 import { Session, SupabaseClient } from "@supabase/supabase-js";
+import { Button } from "../ui/button";
+import { Heart } from "lucide-react";
+import Logo from "../Logo";
 
 type Props = {
   open: boolean;
@@ -17,6 +19,7 @@ type Props = {
   profileName?: string | null;
   supabase: SupabaseClient;
   logoSize?: { w: number; h: number };
+  setFavoritesOpen?: (v: boolean) => void;
 };
 
 export function MobileDrawer({
@@ -28,6 +31,7 @@ export function MobileDrawer({
   profileName,
   supabase,
   logoSize = { w: 78, h: 49 },
+  setFavoritesOpen = () => { }
 }: Props) {
   useLockBodyScroll(open);
   const ref = useRef<HTMLDivElement>(null);
@@ -37,8 +41,11 @@ export function MobileDrawer({
   return (
     <div ref={ref} className="fixed inset-0 z-40 bg-black/40 flex lg:hidden">
       <div className="bg-white dark:bg-[#030712] w-4/5 max-w-xs h-screen overflow-y-auto p-6 flex flex-col gap-4 shadow-lg">
+
         <div className="flex justify-center mb-2">
-          <Logo size={logoSize} />
+          <div className="relative" style={{ width: logoSize.w, height: logoSize.h }}>
+            <Logo />
+          </div>
         </div>
 
         <nav className="flex flex-col gap-2 text-base text-[#010b62] dark:text-white">
@@ -53,16 +60,28 @@ export function MobileDrawer({
             />
           ))}
         </nav>
+        {session && (
+          <Button
+            variant="outline"
+            onClick={() => {
+              setOpen(false);
+              setTimeout(() => setFavoritesOpen(true), 100);
+            }}
+            className="text-[#010b62] border h-10 hover:text-[#010b62] dark:text-white border-[#010b62] rounded-md hover:bg-[#0969da]/15 dark:hover:bg-[#00afd3]/30 dark:border-white dark:bg-[#0000] dark:hover:text-white transition cursor-pointer"
+          >
+            <Heart className="w-4 h-4" />
+            Favoritos
+          </Button>
+        )}
 
-        <div className="mt-4">
-          <UserMenu
-            session={session}
-            supabase={supabase}
-            profileName={profileName ?? undefined}
-            variant="mobile"
-            onAfterAction={() => setOpen(false)}
-          />
-        </div>
+        <UserMenu
+          session={session}
+          supabase={supabase}
+          profileName={profileName ?? undefined}
+          variant="mobile"
+          onAfterAction={() => setOpen(false)}
+        />
+
       </div>
 
       {/* overlay: primeiro fecha dropdown, depois fecha drawer */}
