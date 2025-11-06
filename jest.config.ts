f48adcd8330baa7abd26deff5ea70890b4ee24d4
khsantos <kaauhheidemann@hotmail.com>
2025-11-06 14:22:12 -0300

    ***making the project ready for tests***

---
+++ jest.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  clearMocks: true,
+  verbose: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  coveragePathIgnorePatterns: ["/node_modules/", "/.next/"],
+  testPathIgnorePatterns: ["/node_modules/", "/.next/"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/$1",
+  },
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  reporters: ["default"],
+  testMatch: ["**/__tests__/**/*.(test|spec).[jt]s?(x)"],
+};
+
+export default config;
